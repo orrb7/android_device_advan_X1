dff4265bfc7afd91cdabac0b7a3b2cb26f8e58f9
Giovanni Ricca <giovanniricca@proton.me>
2025-10-19 02:12:36 +0000

    X1 : Enable ELF checks for `libimsma` Rename libsink library to not conflict with the rust module
    Change-Id: I16028606d42d63525b5ef3130d86279bf35e26e1

--- extract-files.py
+++ extract-files.py
@@ -23,7 +23,9 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService'),
-    'system_ext/lib64/libsink.so': blob_fixup()
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
