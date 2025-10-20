a36b9cd876bf3c9770ffab9561993fefe200c74c
bengris32 <bengris32@protonmail.ch>
2025-10-20 04:31:15 +0000

    X1: Configure vendor logtags Change-Id: I089030c954252cfa6707232e617eaaee9b5f4015 Signed-off-by: alextrack2013 <alextrack2013@gmail.com>

--- device.mk
+++ device.mk
@@ -219,6 +219,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
