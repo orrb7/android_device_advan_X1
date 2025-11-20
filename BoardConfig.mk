f66d721b701aa143cd764fec931ed58eb98df9b0
Mashopy <eliasgheeraert@gmail.com>
2025-11-20 13:43:36 +0000

    X1: Stop passing flag 3 to vbmeta images MediaTek LK is so broken that it can't properly pass AVB info with this. We still boot fine without this anyway and this fix fenrir strong integrity.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,7 +169,6 @@ BOOT_SECURITY_PATCH := 2025-02-05
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
