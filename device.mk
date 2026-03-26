aa0ee26728ad434e5e4e3a2a6cc4d551ff373d8d
Rizky <gameorrb9@gmail.com>
2026-03-27 05:38:56 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -258,6 +258,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
